6dd8508b52c678b74ddcf71b0e4b00cb33de4d8d
Anil <adabas@cheetahnetworks.com>
2024-02-12 22:33:26 -0500

    finished project

--- src/components/GoalCard.tsx
+++ src/components/GoalCard.tsx
@@ -1,21 +1,22 @@
 import { Grid, Typography, Card, CardHeader, CardContent } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { Goal } from '../TypeDefinitions/typeDefinition';
 
-export default function GoalCard() {
+export default function GoalCard(props: {goal: Goal; deleteGoalFromList: (id: number) => void;}) {
     return (
         <Card sx={{minWidth: '200px', height: '100px', padding: '5px', backgroundColor: 'black', color: 'white', margin: '0px 0px 5px 5px'}}>
             <CardHeader
                 action={
                   <IconButton color='primary' aria-label="delete">
-                    <DeleteIcon />
+                    <DeleteIcon onClick={() => {props.deleteGoalFromList(props.goal.id)}}/>
                   </IconButton>
                 }
-                title="my goal" 
+                title={props.goal.title} 
             />
             <CardContent>
               <Typography>
-                sample....
+                {props.goal.description}
               </Typography>
             </CardContent>
         </Card>        
--- src/components/GoalList.tsx
+++ src/components/GoalList.tsx
@@ -1,12 +1,17 @@
 import { Grid } from '@mui/material';
 import GoalCard from './GoalCard';
+import { Goal } from '../TypeDefinitions/typeDefinition';
 
-function GoalList() {
+function GoalList(props: {goalList: Goal[]; deleteGoalFromList: (id: number) => void;}) {
     return (
         <Grid item container xs={12} sx={{backgroundColor: 'white', borderRadius: 2, marginTop: '7px', justifyContent: 'space-between', padding: '16px'}}>
-            <Grid item>
-                <GoalCard />
-            </Grid>           
+            {props.goalList.map((goal) => {
+                return (
+                    <Grid item key={goal.id}>
+                        <GoalCard goal={goal} deleteGoalFromList={props.deleteGoalFromList}/>
+                    </Grid>
+                );
+            })}           
         </Grid>
     );
 }
